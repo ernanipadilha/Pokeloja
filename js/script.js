fa8a7b6e6dd65ac605b76443ed85495601b78aa9
ernanipadilha <ernanipadilha@gmail.com>
2021-12-08 12:03:38 -0300

    Botão de página e informação de qual página está.

--- js/script.js
+++ js/script.js
@@ -18,38 +18,58 @@ class Pokemon {
         return pokeDiv;
     }
 }
-let page = 0
-async function getPokemons(page = 0) {
+let page = 0;
+async function getPokemons(page) {
     const limit = 20;
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}$offset=${limit * page}`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${limit * page}`);
     const json = await response.json();
-
     const pages = Math.ceil(json.count / limit);
-
+    
     return json;
+}
+
+async function calcPages(page){
+    const results = await getPokemons();
+    const pages = Math.ceil(results.count /20);
+    const pags = document.querySelector('.pages');
+    pags.innerHTML = `${page+1}/${pages}`
 }
 
 function temAnterior(page) {
     const btnAnt = document.querySelector('.btn-ant');
-
-    if (page === 0) btnAnt.style.visibility = 'hidden';
+    if (page === 0){
+     btnAnt.style.visibility = 'hidden';
+    }else {
+        btnAnt.style.visibility = 'visible'
+    }
 }
 
 function btnProx() {
     const btnProx = document.querySelector('.btn-prox');
 
-    btnProx.onclick = async() => {
+    btnProx.onclick = async () => {
         const response = await getPokemons(page += 1);
-        
         listaPokemons(response.results);
+        temAnterior(page);
+        calcPages(page)
     }
-
-
 }
 
-function listaPokemons(pokemonsApi){
+function btnAnt(){
+    const btnAnt = document.querySelector('.btn-ant');
+
+    btnAnt.onclick = async () => {
+        const response = await getPokemons(page -= 1);
+        listaPokemons(response.results);
+        temAnterior(page)
+        calcPages(page)
+    }
+}
+
+function listaPokemons(pokemonsApi) {
     const pokeList = document.querySelector('.content');
+    pokeList.innerHTML = '';
     const pokemons = pokemonsApi.map((pokemon) => new Pokemon(pokemon.name, pokemon.url));
 
     pokemons.forEach((pokemon) => {
@@ -62,8 +82,9 @@ function listaPokemons(pokemonsApi){
 // Executa quando a página termina de carregar.
 window.onload = async () => {
     const response = await getPokemons(page);
-
     listaPokemons(response.results);
     temAnterior(page);
+    calcPages(page);
     btnProx();
+    btnAnt()
 }
