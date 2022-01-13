63832c62ce6b5166286a944717c6586031d3731b
ernanipadilha <ernanipadilha@gmail.com>
2022-01-12 21:18:02 -0300

    Finalização do localStorage

--- js/carrinho.js
+++ js/carrinho.js
@@ -3,16 +3,17 @@ class Carrinho {
     btnFecharCarrinho = document.querySelector('#fechar-carrinho');
     clickForaCart = document.querySelector('.open-cart');
     meuStorage = localStorage;
-    itens = [];
     total = 0;
+
     constructor() {
         this.btnCarrinho.addEventListener('click', this.abrirCarrinho);
         this.btnFecharCarrinho.addEventListener('click', this.fecharCarrinho);
         this.clickForaCart.addEventListener('click', this.fecharCarrinho);
     }
+
     abrirCarrinho(event) {
         event.preventDefault();
-        
+        window.carrinho.renderCarrinho();
         const openCartClass = 'carrinho-aberto'
         document.body.className.includes(openCartClass) ? document.body.className = '' : document.body.className = openCartClass;
         addEventListener('keydown', function (event) {
@@ -29,27 +30,27 @@ class Carrinho {
         event.preventDefault();
         document.body.className = '';
     }
-    
+
     adicionar(pokemon) {
-        this.itens.push(pokemon);
-        this.renderCarrinho();
-        this.populateStorage();
+        this.populateStorage(pokemon);
     }
 
     renderCarrinho() {
+        const pokemons = this.getStorage();
+        
         const pokeCar = document.querySelector('.poke-car');
         pokeCar.innerHTML = '';
 
         const pokeTotais = document.querySelector('.poke-total')
         pokeTotais.innerHTML = '';
         let precoTotal = 0;
-        let pokemonsComprados = this.itens.map((itens, index) => {
-                const qtdPoke = index + 1;
-                precoTotal = precoTotal + parseFloat(itens.precoDesc);
-                console.log(precoTotal);
-                const pokeItem = document.createElement('div');
-                pokeItem.className = 'poke-data';
-                pokeItem.innerHTML = `
+        let pokemonsComprados = pokemons.map((itens, index) => {
+            const qtdPoke = index + 1;
+            precoTotal = precoTotal + parseFloat(itens.precoDesc);
+            console.log(precoTotal);
+            const pokeItem = document.createElement('div');
+            pokeItem.className = 'poke-data';
+            pokeItem.innerHTML = `
             <img class="poke-item-car"
             src="${itens.image}"
             alt="${itens.nome}">
@@ -59,33 +60,28 @@ class Carrinho {
             <button>Excluir</button>
             </div>
             <p class="price">R$ ${itens.precoDesc}</p>`;
-                pokeCar.appendChild(pokeItem);
+            pokeCar.appendChild(pokeItem);
 
-                const pokeTotal = document.createElement('div');
-                pokeTotal.className = 'total-cart';
-                pokeTotal.innerHTML = ` 
+            const pokeTotal = document.createElement('div');
+            pokeTotal.className = 'total-cart';
+            pokeTotal.innerHTML = ` 
             <h4>Total itens: ${qtdPoke}</h4>
             <h4>Valor total: R$ ${(precoTotal).toFixed(2)}</h4>`;
 
-                pokeTotais.innerHTML = '';
-                pokeTotais.appendChild(pokeTotal);
-            });
-    }
-    populateStorage(){
-        localStorage.setItem("pokemonsNoCarrinho", JSON.stringify(window.carrinho.itens));
-        console.log(localStorage)
+            pokeTotais.innerHTML = '';
+            pokeTotais.appendChild(pokeTotal);
+        });
     }
 
-     getStorage(){
-        let meusPokemons = JSON.parse(localStorage.getItem("pokemonsNoCarrinho"));
-        reloadStorage()
+    populateStorage(pokemon) {
+        const pokemons = this.getStorage() || [];
+        pokemons.push(pokemon);
+        localStorage.setItem("pokemonsNoCarrinho", JSON.stringify(pokemons));
     }
 
-    reloadStorage(meusPokemons){
-        localStorage.setItem("pokemonsNoCarrinho", JSON.stringify(meusPokemons));
-        renderCarrinho();
+    getStorage() {
+        return JSON.parse(localStorage.getItem("pokemonsNoCarrinho"));
     }
-    
 }
 
 /*
@@ -99,4 +95,5 @@ Abrir o carrinho
 
 window.addEventListener('load', async () => {
     window.carrinho = new Carrinho();
-   });
+
+});
